178c2160dfc09e90ce861bb16e158ce43fd645ba
NicoleOrmerod94 <147579584+NicoleOrmerod94@users.noreply.github.com>
2024-05-19 18:56:48 -0600

    Scope lab with changing variables

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+var customerName = 'bob';
+//declare customer name to be bob in global scope
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}   
+//add modification to customerName variable. toUpperCase() is a method that changes the string to uppercase
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+// Declare bestCustomer as a global variable
+function overwriteBestCustomer() {
+  bestCustomer  = 'maybe bob';
+}
+// Function to overwrite bestCustomer
+const leastFavoriteCustomer = 'some initial value';
+// Declare leastFavoriteCustomer as a constant
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'new value'; // This should throw an error
+}
+// Function attempting to change the constant
